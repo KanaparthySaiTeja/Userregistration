e95108364b608313b82a2a12dc4f3848ed2ee003
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-04 23:23:08 +0530

    Valid First Name

--- src/main/java/com/Userregistration/Userregistration.java
+++ src/main/java/com/Userregistration/Userregistration.java
@@ -1,13 +1,26 @@
 package com.Userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
  */
 public class Userregistration
 {
+	static Scanner sc=new Scanner(System.in);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello" );
+      firstName();
     }
+    public static void firstName() {
+		System.out.println("Enter First Name");
+		String fN = sc.nextLine();
+		if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", fN) == true)
+			System.out.println("Valid First Name");
+		else
+			System.out.println("Invalid First Name");
+	}
 }
