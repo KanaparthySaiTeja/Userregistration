d8231034f35b4a6ca3e0f772e8bea0316eaa2c97
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-05 08:52:50 +0530

    email validation

--- src/main/java/com/Userregistration/Userregistration.java
+++ src/main/java/com/Userregistration/Userregistration.java
@@ -1,37 +1,27 @@
 package com.Userregistration;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
+
 
 /**
  * Hello world!
  *
  */
-public class Userregistration
-{
-	static Scanner sc=new Scanner(System.in);
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello" );
-      firstName();
-      lastName();
-    }
-    //UC1 Valid first name
-    public static void firstName() {
-		System.out.println("Enter First Name");
-		String fN = sc.nextLine();
-		if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", fN) == true)
-			System.out.println("Valid First Name");
+public class UserRegistration {
+	
+	public static void email(String emailId) {
+		String regex = "^[_a-z0-9-\\+]+(\\.[_a-z0-9-]+)*@[a-z0-9]+(\\.[a-z0-9]+)*(\\.[a-z]{2,})$";
+		if (emailId.matches(regex))
+			System.out.println("EmailId is Valid");
 		else
-			System.out.println("Invalid First Name");
+			System.out.println("EmailId is Invalid");
 	}
- // UC2 - Last name starts with Cap and has minimum 3 characters
- 	public static void lastName() {
- 		System.out.println("Enter Last Name");
- 		String lN = sc.nextLine();
- 		if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lN) == true)
- 			System.out.println("Valid Last Name");
- 		else
- 			System.out.println("Invalid Last Name");
- 	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the EmailId");
+		String emailId = sc.nextLine();
+		email(emailId);
+		sc.close();
+	}
+
 }
