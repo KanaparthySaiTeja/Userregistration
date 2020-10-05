0abd1d6d6e9ddc94d906bc6480742a717697ea85
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-05 09:05:37 +0530

    special character validation

--- src/main/java/com/Userregistration/Userregistration.java
+++ src/main/java/com/Userregistration/Userregistration.java
@@ -9,20 +9,20 @@ import java.util.Scanner;
  */
 public class UserRegistration {
 	
-public static void setPassword(String password) {
-	String regex = "^(?=.*[A-Z]).{8,}$";
-	if (password.matches(regex))
-		System.out.println("Mobile Number is Valid");
-	else
-		System.out.println("Mobile Number is Invalid");
-}
+	public static void setPassword(String password) {
+		String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+		if (password.matches(regex))
+			System.out.println("Mobile Number is Valid");
+		else
+			System.out.println("Mobile Number is Invalid");
+	}
 
-public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the password");
-	String password = sc.nextLine();
-	setPassword(password);		
-	sc.close();
-}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the password");
+		String password = sc.nextLine();
+		setPassword(password);		
+		sc.close();
+	}
 
 }
